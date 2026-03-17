55a1f3f1f6cf8b490e7dbb9a6bd72cebbe48d661
Robert Vihodita <robert.vihodita@stud.ubbcluj.ro>
2026-03-17 19:44:49 +0200

    projectile firing system

--- model/Projectile.js
+++ model/Projectile.js
@@ -1,10 +1,20 @@
-class Projectile extends Entity {
+class Projectile {
     constructor(x, y) {
-        super(x, y, 5, 15, 'yellow');
-        this.speed = 10;
+        this.x = x;
+        this.y = y;
+        this.width = 4;
+        this.height = 10;
+        this.speed = 7;
+        this.markedForDeletion = false;
     }
 
     update() {
-        this.y -= this.speed; // Merge în sus
+        this.y -= this.speed;
+        if (this.y < 0) this.markedForDeletion = true;
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = 'yellow';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
